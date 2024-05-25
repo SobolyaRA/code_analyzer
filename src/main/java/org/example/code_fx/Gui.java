d8c980ecba8914acf730b9f2178d195683afca0e
ilia14042001@gmail.com <81635546+SobolyaRA@users.noreply.github.com>
2024-05-25 20:58:23 +0500

    v2

---
+++ src/main/java/org/example/code_fx/Gui.java
@@ -0,0 +1,25 @@
+package org.example.code_fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class Gui extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("logo.jpg")));
+        primaryStage.setTitle("CodeAnalyzer");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/org/example/code_fx/Main.java
+++ src/main/java/org/example/code_fx/Main.java
@@ -1,25 +1,7 @@
 package org.example.code_fx;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("logo.jpg")));
-        primaryStage.setTitle("CodeAnalyzer");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Gui.main(args);
     }
 }
